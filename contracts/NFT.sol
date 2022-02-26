3305a61efcdf651f702f2854cb3febcb7a42f1db
indrajeetnikamcf <indrajeet.nikam@cashfree.com>
2022-02-26 14:53:25 +0530

    feat: add NFT Market contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,9 +1,9 @@
-// SPDX-Licence-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
