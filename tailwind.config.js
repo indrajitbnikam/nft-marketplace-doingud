65118b77623cad064dd4a0325658cc5cbdd0add8
indrajeetnikamcf <indrajeet.nikam@cashfree.com>
2022-02-26 18:44:40 +0530

    style: more tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: [],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/line-clamp')],
+};
